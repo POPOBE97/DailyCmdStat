715650b6b752748b12e8230b80c0e881d1012d17
ruiyao <luoruiyao@126.com>
2020-03-05 17:23:19 +0100

    Initial Push

---
+++ CmdStat.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import json
+import os, sys, re
+import requests
+
+HISTFILE = sys.argv[1]
+URL      = "https://ss.luoruiyao.cn/api/upload_cmd_stat.php"
+REG = r"^[\s]*([^\s]*).*$"
+SEL = r"^.*CmdStat(.py)?\s[\S]*$"
+
+f = open(HISTFILE, 'r')
+lines = f.readlines()
+f.close()
+
+CMDS  = {}
+for i in range(len(lines)-1, -1, -1):
+    line = lines[i]
+    if re.search(SEL,line):
+        break
+    elif len(line):
+        cmds  = re.findall(REG, line)
+        for cmd in cmds:
+            try:
+                CMDS[cmd] += 1
+            except KeyError:
+                CMDS[cmd] = 1
+    else: continue
+
+r = requests.post(URL, json={"content": CMDS})
+
+
